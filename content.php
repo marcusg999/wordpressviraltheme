aeed4ef7b03abacb9d164c70d9003c15c4983b18
marcus gray <abundanceisfree@gmail.com>
2016-09-09 23:53:59 -0700

    must fix footer

--- content.php
+++ content.php
@@ -1,87 +1,78 @@
 <!-- Main Video-->
+<div class="row">
+    <div class="column-left">
+        <div class="col-xs-12 col-sm-6 col-md-8">
+        <div class="mainVideo">
+          <div class="container">
+              <iframe width="420" height="245" src="https://www.youtube.com/watch?v=Wb8KpHqU5tg">
+              </iframe>
+          </div>
+      <div class="blog-post">
+        <h2 class="blog-post-title"><?php the_title(); ?></h2>
+        <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
-<div class="mainVideo">
-  <div class="container">
-      <iframe width="420" height="245" src="https://www.youtube.com/watch?v=Wb8KpHqU5tg">
-      </iframe>
-  </div>
-<div class="blog-post">
-  <h2 class="blog-post-title"><?php the_title(); ?></h2>
-  <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+       <?php the_content(); ?>
 
- <?php the_content(); ?>
-
-</div><!-- /.blog-post -->
+      </div><!-- /.blog-post -->
 
 <!-- Archives -->
-<h4>Archives</h4>
-<ol class="list-unstyled">
-  <?php wp_get_archives('type=monthly'); ?>
-</ol>
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <?php wp_get_archives('type=monthly'); ?>
+            </ol>
 
-<!-- ul with images should this stay in content.php? -->
+            <!-- ul with images should this stay in content.php? -->
 
-    <ul>
-        <li class="service-list">
-            <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
-            <h3></h3>
-            <p>
-            The Best Labor Day Deals
-            </P>
-        </li>
-    </ul>
-    <ul>
-    <li class="service-list">
-        <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
-        <h3></h3>
-        <p>
-        The Best Labor Day Deals
-        </P>
-    </li>
-    </ul>
-    <ul>
-    <li class="service-list">
-        <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
-        <h3></h3>
-        <p>
-        The Best Labor Day Deals
-        </P>
-    </li>
-    </ul>
-    <ul>
-    <li class="service-list">
-        <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
-        <h3></h3>
-        <p>
-        The Best Labor Day Deals
-        </P>
-    </li>
-    </ul>
-    <ul>
-    <li class="service-list">
-        <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
-        <h3></h3>
-        <p>
-        The Best Labor Day Deals
-        </P>
-    </li>
-    </ul>
-    <ul>
-    <li class="service-list">
-        <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
-        <h3></h3>
-        <p>
-        The Best Labor Day Deals
-        </P>
-    </li>
-    </ul>
-    <ul>
-    <li class="service-list">
-        <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
-        <h3></h3>
-        <p>
-        The Best Labor Day Deals
-        </P>
-    </li>
-</ul>
-</div>
+                <ul>
+                    <li class="service-list">
+                        <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
+                        The Best Labor Day Deals
+                    </li>
+                </ul>
+                <ul>
+                <li class="service-list">
+                    <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
+                    The Best Labor Day Deals                </li>
+                </ul>
+                <ul>
+                <li class="service-list">
+                    <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
+                    The Best Labor Day Deals
+                </li>
+                </ul>
+                <ul>
+                <li class="service-list">
+                    <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
+                    The Best Labor Day Deals
+                </li>
+                </ul>
+                <ul>
+                <li class="service-list">
+                    <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
+                    The Best Labor Day Deals
+                </li>
+                </ul>
+                <ul>
+                <li class="service-list">
+                    <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
+                    The Best Labor Day Deals
+                </li>
+                </ul>
+                <ul>
+                <li class="service-list">
+                    <a href=""><img src="image.png" alt="icon" class="alignnone size-full wp-image-156" /></a>
+                    The Best Labor Day Deals
+                </li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  <div class="AdRight">
+      <div class = "AdFrame">
+          <div class="col-xs-6 col-md-4"><img src="image.png" alt="icon" /></div>
+      </div>
+  </div>
+
+
+
+
--- footer.php
+++ footer.php
@@ -7,10 +7,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
 <?php wp_footer(); ?>
-  </body>
-</html>
 
     <h4></h4>
     <ol class="list-unstyled">
@@ -18,5 +15,4 @@
       <li><a href="#">Twitter</a></li>
       <li><a href="#">Facebook</a></li>
     </ol>
-  </div>
-</div>
+
